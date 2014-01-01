b9749867cdc8af8afabf9190c3380f0e7d6c6ea6
Scinart <akubeej@gmail.com>
2014-01-01 14:28:53 +0800

    ignore elpa folder

--- elpa/bookmark+-20131007.2334/bookmark+-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "bookmark+" "20131007.2334" "Bookmark+: extensions to standard library `bookmark.el'. [wiki]" 'nil)
-
--- elpa/pretty-mode-plus-1.2/pretty-mode-plus-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; pretty-mode-plus-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("pretty-mode-plus-pkg.el" "pretty-mode-plus.el")
-;;;;;;  (21091 35702 694052 128000))
-
-;;;***
-
-(provide 'pretty-mode-plus-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; pretty-mode-plus-autoloads.el ends here
